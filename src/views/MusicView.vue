3bd0e906de82405473398e4c4880586e7ea2d1d8
Ibrahim <ibrahimomer89@gmail.com>
2022-10-24 16:41:18 +0200

    Add Music view

---
+++ src/views/MusicView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="dasds">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+  .dasds{
+    background-color: var(--highlight-color1);
+    border-radius: 2rem;
+  }
+</style>
